a1adc9932ee0ea8adce4ad77e30e087ed9d51e2a
August <augustwiesner16@gmail.com>
2022-08-13 14:23:55 -0500

    Completed intial styles of main habits. No functionality yet

--- src/components/WeeklyHabits.js
+++ src/components/WeeklyHabits.js
@@ -3,13 +3,37 @@ import React from 'react'
 const WeeklyHabits = () => {
   return (
     <div id='weekly'>
-        <div className='weekly-habit hydration'>Hydration</div>
-        <div className='weekly-habit'>Bruh</div>
-        <div className='weekly-habit'>Bruh</div>
+        <div className='weekly-habit hydration'>
+          <p>Hydration</p>
+          <div className='weekly-habit-number'>0/2</div>
+          <div className='weekly-habit-desc'>Drinking enough water per day</div>
+        </div>
+        <div className='weekly-habit'>
+          <p>Exercise</p>
+          <div className='weekly-habit-number'>0/3</div>
+          <div className='weekly-habit-desc'>Increasing physical strength</div>
+        </div>
+        <div className='weekly-habit'>
+          <p>Mental</p>
+          <div className='weekly-habit-number'>0/4</div>
+          <div className='weekly-habit-desc'>Controlling your mind</div>
+        </div>
 
-        <div className='weekly-habit'>Bruh</div>
-        <div className='weekly-habit'>Bruh</div>
-        <div className='weekly-habit'>Bruh</div>
+        <div className='weekly-habit'>
+          <p>Eating</p>
+          <div className='weekly-habit-number'>0/2</div>
+          <div className='weekly-habit-desc'>Eat well</div>
+        </div>
+        <div className='weekly-habit'>
+        <p>Working</p>
+          <div className='weekly-habit-number'>0/2</div>
+          <div className='weekly-habit-desc'>Working hard everyday</div>
+        </div>
+        <div className='weekly-habit'>
+        <p>Social</p>
+          <div className='weekly-habit-number'>0/3</div>
+          <div className='weekly-habit-desc'>Levelling up social skills</div>
+        </div>
 
     </div>
   )
