f9fed15895404af16cd6b5676c59a2570db509ec
August <augustwiesner16@gmail.com>
2022-10-12 10:58:16 -0500

    figuring out how to add elements with button.

--- src/components/DailyHabitsMenu.js
+++ src/components/DailyHabitsMenu.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 
 /* const buttons = document.getElementsByClassName('dh-button');
 for (let i = 0; i < buttons.length; i++) {
@@ -26,16 +26,48 @@ const DailyHabitsMenu = () => {
     })
   }
 
-  const hydration = useRef()
+  const hydration = useRef();
+
+  const [name, setName] = useState('');
 
   useEffect(() => {
     hydration.current = document.getElementsByClassName("hydration").length;
     document.getElementById("hydration").innerText = `0/${hydration.current}`;
   })
   
+  const addTask = () => {
+    let habit = document.createElement('div');
+    let selected = document.getElementById("select");
+    habit.setAttribute(`className`, `dh-habit ${selected.value}`)
+    habit.innerText = `${name}`
+
+    document.getElementById("dailyhabitsmenu").appendChild(habit)
+  }
  
   return (
     <div id='dailyhabitsmenu'>
+        <div className='new-task'>
+          <h3>Add a habit</h3>
+          <div className='new-task-form'>
+            <input className='new-task-input' 
+              placeholder='Task name'
+              onChange={(e) => {
+                setName(e.target.value)}}
+            ></input>
+            <select id='select'>
+              <option disabled selected >Category:</option>
+              <option value="hydration">Hydration</option>
+              <option value="exercise">Exercise</option>
+              <option value="eating">Eating</option>
+              <option value="working">Working</option>
+              <option value="mental">Mental</option>
+              <option value="social">Social</option>
+            </select>
+            <button id='new-task-button' onClick={() => {addTask()}}>Add a new task</button>
+          </div>
+        </div>
+
+
         <div className='dh-habit'>
             <button className='dh-button hydration' onClick={(e) => {buttonClick(e, "hydration")}}></button>
             <div>Drink 1 glass after waking</div>
